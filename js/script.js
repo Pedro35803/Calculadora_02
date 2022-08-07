f74ca6e5e7d71284169104ef769abe024988b725
Pedro <pedrojuniorifpb@gmail.com>
2022-08-07 11:28:00 -0300

    Inicio da parte logica

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
+// change tema da pagina
+
 document.querySelector("#change_theme").addEventListener("click", () => {
     const page = document.querySelector("body");
     page.classList.toggle("theme_dark");
 });
+
+// Logica da calculadora
+
+const expression = document.querySelector("#expression");
+const result = document.querySelector("#result");
+
+const btNumbers = document.querySelectorAll(".button-number");
+const btFunctions = document.querySelectorAll(".button-function");
+
+let list = [];
+
+btNumbers.forEach(btNumber => btNumber.addEventListener("click", () => {
+        const buttonContent = btNumber.textContent;
+        concatedNumber(buttonContent);
+    }
+));
+
+btFunctions.forEach(btFunction => btFunction.addEventListener("click", () => {
+        const buttonContent = btFunction.textContent;
+        concatedFunction(buttonContent);
+    }
+));
+
+function concatedNumber(number) {
+    expression.value += number;
+    const sizeList = list.length - 1;
+    list[sizeList] += number;
+}
+
+function concatedFunction(string) {
+    const sizeList = list.length - 1;
+    if (list[sizeList] != "" && Number(list[sizeList]) != NaN) {
+        expression.value += " " + string + " ";
+        list.push(string);
+        list.push("");
+    }
+}
